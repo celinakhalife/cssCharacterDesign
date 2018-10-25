d8f102568d76ed1103aa53dcd2adaea26db40dfb
adsingh14 <deep.amansingh14@gmail.com>
2018-10-26 02:53:55 +0530

    Added Dynamic-Stars background, planet elements animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+
 export class AppComponent {
-  title = 'alien';
+  title = 'alien',
+  tinyStars = new Array(500),
+  stars = new Array(20),
 }
